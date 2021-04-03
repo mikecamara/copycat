5b508bbb6d303c5a8d41e190839db738a27b3341
mike83 <mike.gomes.camara@ut.ee>
2021-04-03 19:53:55 +0300

    Add changes

---
+++ src/styles.js
@@ -0,0 +1,31 @@
+const fontFamily = 'Comic Sans MS, Lucida Handwriting, cursive';
+const fontSize = '5vh';
+const backgroundColor = '#282c34';
+const display = 'flex';
+const flexDirection = 'column';
+const alignItems = 'center';
+const justifyContent = 'center';
+const color = 'white';
+const marginTop = '20px';
+const widthImage = '10%';
+
+const divStyles = {
+  fontFamily: fontFamily,
+  fontSize: fontSize,
+  color: color,
+  backgroundColor: backgroundColor,
+  display: display,
+  flexDirection: flexDirection,
+  alignItems: alignItems,
+  justifyContent: justifyContent,
+};
+
+const imgStyles = {
+  marginTop: marginTop,
+  width: widthImage
+};
+
+export const styles = {
+    divStyles: divStyles,
+    imgStyles: imgStyles
+}
